561481f31e2df37cf8089feddc890077fb041904
Cooltie1 <scottolsonofficial@gmail.com>
2021-12-03 12:10:37 -0700

    Made the search work

--- opusapp/views.py
+++ opusapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
-from opusapp.models import State, Prescriber
+from opusapp.models import State, Prescriber, Drug
+from django.db.models.functions import Concat
+from django.db.models import Value, F
 
 # Create your views here.
 def indexPageView(request) :
@@ -19,15 +21,44 @@ def searchPageView(request) :
     return render(request, 'opusapp/search.html', context)
 
 def resultsPrescriberView(request) :
-    return render(request, 'opusapp/search-results.html')
+    fname = request.GET['inputFname']
+    lname = request.GET['inputLname']
+    credentials = request.GET['inputCredentials']
+    gender = request.GET['inputGender']
+    state = request.GET['inputState']
+    specialty = request.GET['inputSpecialty']
+    data = Prescriber.objects.values('npi', 'fname', 'lname'
+    ).annotate(id=F('npi'), name=Concat('lname', Value(', '), 'fname')
+    ).filter(fname__icontains=fname, lname__icontains=lname, credential__icontains=credentials, gender__icontains=gender, state__state_name__icontains=state, specialty__icontains=specialty )
+    context = {
+        'data' : data,
+        'type' : 'prescriber'
+
+    }
+
+    return render(request, 'opusapp/search-results.html', context)
 
 def resultsDrugView(request) :
-    return render(request, 'opusapp/search-results.html')
+    drugname = request.GET['inputDrugName']
+    data = Drug.objects.values('drug_id', 'drug_name').annotate(id=F('drug_id'), name=F('drug_name'))
+    if 'isOpiate' not in request.GET :
+        data = data.filter(drug_name__icontains=drugname)
+    else :
+        data = data.filter(drug_name__icontains=drugname, is_opioid=True)
+    
+    
+    context = {
+        'data' : data,
+        'type' : 'drug'
+    }
+
+
+    return render(request, 'opusapp/search-results.html', context)
 
 def prescriberView(request, npi) :
     return render(request, 'opusapp/prescriber.html')
 
-def drugView(request, drugid) :
+def drugView(request, drug_id) :
     return render(request, 'opusapp/drug.html')
 
 def learnView(request) :
