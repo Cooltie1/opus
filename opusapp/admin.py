46b01a149e5718223fd5c3fa80b8368039ac913f
Cooltie1 <scottolsonofficial@gmail.com>
2021-12-02 21:32:04 -0700

    fixed search dropdowns and searchPageView function

--- opusapp/admin.py
+++ opusapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.db import models
+from .models import State, Drug, Prescriber, PrescriberDrug
 
 # Register your models here.
+admin.site.register(State)
+admin.site.register(Drug)
+admin.site.register(Prescriber)
+admin.site.register(PrescriberDrug)
+
--- opusapp/views.py
+++ opusapp/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 
+from opusapp.models import State, Prescriber
+
 # Create your views here.
 def indexPageView(request) :
     return render(request, 'opusapp/index.html')
 
 def searchPageView(request) :
-    return render(request, 'opusapp/search.html')
+    credentialsSQL = 'select distinct 1 NPI, credential from prescriber order by credential'
+    specialtySQL = 'select distinct 1 NPI, specialty from prescriber order by specialty'
+    context = {
+        "state" : State.objects.all(),
+        "credentials" : Prescriber.objects.raw(credentialsSQL),
+        "specialty" : Prescriber.objects.raw(specialtySQL)
+    }
+
+    
+    return render(request, 'opusapp/search.html', context)
 
 def resultsPrescriberView(request) :
     return render(request, 'opusapp/search-results.html')
