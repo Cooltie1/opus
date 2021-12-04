35df25eb8f5d1ed7fcbbfe340492b163230a52db
JacksonSchow <80366532+JacksonSchow@users.noreply.github.com>
2021-12-04 14:08:57 -0700

    let's go

--- opusapp/views.py
+++ opusapp/views.py
@@ -89,12 +89,43 @@ def adminView(request, npi=0) :
    
 
 def adminNewView(request) :
-    if request.method == 'POST' :
+    if request.method == 'GET' :
         credentialsSQL = 'select distinct 1 NPI, credential from prescriber order by credential'
         specialtySQL = 'select distinct 1 NPI, specialty from prescriber order by specialty'
         context = {
             "state" : State.objects.all(),
             "credentials" : Prescriber.objects.raw(credentialsSQL),
-            "specialty" : Prescriber.objects.raw(specialtySQL)
+            "specialty" : Prescriber.objects.raw(specialtySQL),
+            'message' : ''
         }
         return render(request, 'opusapp/admin-new.html', context)
+
+    elif request.method == 'POST':
+        prescriber = Prescriber()
+        state = State.objects.get(state_name = request.POST['state'])
+
+        prescriber.npi = request.POST['npi']
+        prescriber.fname = request.POST['fname']
+        prescriber.lname = request.POST['lname']
+        prescriber.credential = request.POST['credential']
+        prescriber.gender = request.POST['gender']
+        prescriber.state = state
+        prescriber.specialty = request.POST['specialty']
+        prescriber.is_opioid_prescriber = False
+        prescriber.total_prescriptions = 0
+
+        prescriber.save()
+
+        credentialsSQL = 'select distinct 1 NPI, credential from prescriber order by credential'
+        specialtySQL = 'select distinct 1 NPI, specialty from prescriber order by specialty'
+
+        context = {
+            'message' : 'Added Successfully',
+            "state" : State.objects.all(),
+            "credentials" : Prescriber.objects.raw(credentialsSQL),
+            "specialty" : Prescriber.objects.raw(specialtySQL)
+        }
+
+        return render(request, 'opusapp/admin-new.html', context)
+    else:
+        return render(request, 'opusapp/index.html')
